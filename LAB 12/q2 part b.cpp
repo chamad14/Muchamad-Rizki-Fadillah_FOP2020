772bc27d57cead4fc857632dacc8751909a3f1ee
repl.it user <replituser@example.com>
2020-12-11 04:53:06 +0000

    ada revisi nomor 2

--- LAB 12/q2 part b.cpp
+++ LAB 12/q2 part b.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int binarySearch(const int list[], int key, int listSize);
-
+void selectionSort(int a[], int n);
 int main()
 {
-  int list[] = {1, 2, 3, 4, 5, 6, 7};
+  int list[] = {1, 4, 7, 2 , 6 , 3 , 5 };
   int x;
+  int n = sizeof(list)/sizeof(list[0]);
+  selectionSort (list,n);
   cout << "choose integer from 1- 7" << endl;
   cin >> x;
   cout << binarySearch(list, x, 7);
@@ -31,4 +33,17 @@ int binarySearch(const int list[], int key, int listSize)
   }
 
   return -min - 1;
+}
+
+void selectionSort(int a[], int n) {
+   int i, j, min, temp;
+   for (i = 0; i < n - 1; i++) {
+      min = i;
+      for (j = i + 1; j < n; j++)
+      if (a[j] < a[min])
+      min = j;
+      temp = a[i];
+      a[i] = a[min];
+      a[min] = temp;
+   }
 }
